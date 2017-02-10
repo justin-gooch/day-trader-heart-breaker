1c1f7b8b2712be6bda39c9b19135af025835d817
JacobWoofly <barkley@berkeley.edu>
2017-02-09 21:17:36 -0800

    Lemme know all the errors! lol

--- quoteGrabber.py
+++ quoteGrabber.py
@@ -20,3 +20,30 @@
 # my suggestion is to make the table with an id (for referencing elsewhere that auto increments), stock symbol name,
 # stock current price and unix timestamp
 
+import urllib2
+import sqlite3
+import datetime, time
+
+symbol='ATVI'
+info='abo'
+
+today = datetime.date.today()
+
+for i in symbol
+	link=('http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s,symbol(i),info(i)')
+	now = datetime.datetime.now()
+	data(i) = 'today, now, urllib2.urlopen(link)'
+
+conn = sqlite3.connect('Stocks.db')
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE stocks
+             (date text, time text, symbol text, ask real, bid real, open real)''')
+
+# Insert the data
+c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', data)
+
+# Save (commit) the changes
+conn.commit()
+conn.close()
